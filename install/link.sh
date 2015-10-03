f7ab22fa9c066bd5eb6384855a02a31b67904a1a
Alexander Kovalchuk <kovalexal@gmail.com>
2015-10-03 15:14:17 +0300

    Added vimrc and linking it during installation

--- install/link.sh
+++ install/link.sh
@@ -10,3 +10,7 @@ ln -is $DOTFILES/zsh/zshrc $HOME/.zshrc
 echo "Linking tmux"
 ln -is $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
 
+echo "Linking vim"
+ln -is $DOTFILES/vim/vimrc $HOME/.vimrc
+ln -is $DOTFILES/vim/vim $HOME/.vim
+
